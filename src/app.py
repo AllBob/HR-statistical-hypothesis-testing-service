d39a833c340a4571bbccdc2106de2ec3ca5a02dd
Alla Bo <allabo@MacBook-Air.local>
2023-12-11 13:26:50 +0300

    minor changes2

--- src/app.py
+++ src/app.py
@@ -75,7 +75,7 @@ if uploaded_file is not None:
             st.markdown( "<h3 style='text-align: left; color: red;'>Объем выборки (выборок) недостаточен для проверки гипотезы </h3>", unsafe_allow_html=True)
         else:
             if (len(data_male)>=3 and len(data_male)<20) or (len(data_female)>=3 and len(data_male)<20):
-                st.text(':exclamation: Предупреждение: малый объем выборок может повлиять на корректность результата')
+                st.write(':exclamation: :exclamation: :exclamation: Предупреждение: малый объем выборок может повлиять на корректность результата')
             ## Графики распределений
             tab1, tab2, tab3 =st.tabs(["Гистограмма",  "Распределение мужчины", "Распределение женщины"])
             with tab1:
@@ -122,7 +122,7 @@ if uploaded_file is not None:
             st.markdown( "<h3 style='text-align: left; color: red;'>Объем выборки (выборок) недостаточен для проверки гипотезы </h3>", unsafe_allow_html=True)
         else:
             if (len(data_aged)>=3 and len(data_aged)<20) or (len(data_young)>=3 and len(data_young)<20):
-                st.text(":exclamation: Предупреждение: малый объем выборок может повлиять на корректность результата")
+                st.write(':exclamation: :exclamation: :exclamation: Предупреждение: малый объем выборок может повлиять на корректность результата')
             # Графики распределений
             tab1, tab2, tab3 =st.tabs(["Гистограмма",  "Распределение старше порога", "Распределение моложе порога"])
 
